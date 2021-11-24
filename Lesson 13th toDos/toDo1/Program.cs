1e6b2322f42b77a1bed03c566ce5b1f96fc67d31
KamranZadeh <Shixalizadekamran@gmail.com>
2021-11-24 13:15:35 +0400

    lesson 13

---
+++ Lesson 13th toDos/toDo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace toDo1
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte driveCount = 0;
+
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                driveCount++;
+            }
+
+            Console.WriteLine($"This computer has {driveCount} drives\n");
+
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+
+                Console.WriteLine($"The {drive.Name} drive has {drive.TotalSize/(1024*1024)} MB total size\n" +
+                    $"{drive.AvailableFreeSpace/(1024*1024)} MB aviable free space.\n" +
+                    $"And {(drive.TotalSize-drive.AvailableFreeSpace)/(1024*1024)} MB is in use\n");
+
+            }
+        }
+    }
+}
---
+++ Lesson 13th toDos/toDo2and3/Program.cs
@@ -0,0 +1,47 @@
+﻿using Amazon.DirectoryService.Model;
+using Microsoft.VisualBasic;
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace toDo2and3
+{
+    class Program
+    {
+        static void Main()
+        {
+            GetRamInfo();
+            GetCpuInfo();
+        }
+        
+        public static void GetRamInfo()
+        {
+            var searcher = new ManagementObjectSearcher(new ObjectQuery("SELECT * FROM Win32_OperatingSystem"));
+            var results = searcher.Get();
+
+            foreach (var result in results)
+            {
+                var total = result["TotalVisibleMemorySize"]; // find total space
+                var free = result["FreePhysicalMemory"]; // find free space
+                var t = Convert.ToInt32(total) / 1024f; // convert byte to Mb
+                var f = Convert.ToInt32(free) / 1024f; // convert byte to Mb
+                Console.WriteLine($"Total RAM size is {Math.Round(t)} MB ");
+                Console.WriteLine($"Using RAM memory is {Math.Round(t-f)} MB");
+            }
+
+        }
+
+        public static void GetCpuInfo()
+        {
+            PerformanceCounter cpu = new("Processor", "% Processor Time", "_Total");
+            cpu.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine($"Current CPU usage is {Math.Round(cpu.NextValue())}%");
+        }
+    }
+    
+}
