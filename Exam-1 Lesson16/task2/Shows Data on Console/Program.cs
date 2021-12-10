b7135c6dfbebd8721f3ccb1e4d8f47c810a360c0
KamranZadeh <Shixalizadekamran@gmail.com>
2021-12-10 21:43:41 +0400

    Exam added

---
+++ Exam-1 Lesson16/task2/Shows Data on Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Shows_Data_on_Console
+{
+    class Program
+    {
+        static void  Main()
+        {
+            const string myFile = @"D:\myFolder\documents\StudentFile.doc";
+            string newAddedStudents;
+
+
+            while (true)
+            {
+                Thread.Sleep(2000);
+                string[] lines = File.ReadAllLines(myFile);
+                int n = lines.Length - 1;
+
+                Console.WriteLine($"{lines[n - 3]}\n" +
+                    $"{lines[n - 2]}\n" +
+                    $"{lines[n - 1]}\n");
+
+                newAddedStudents = File.ReadAllText(myFile);
+
+                while (true)
+                {
+                    Thread.Sleep(2000);
+                    if (newAddedStudents != File.ReadAllText(myFile))
+                    {
+                        Thread.Sleep(2000);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
