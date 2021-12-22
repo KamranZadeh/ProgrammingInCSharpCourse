f82a7d20277f2fd85e5599922fe939d51dc49992
KamranZadeh <Shixalizadekamran@gmail.com>
2021-12-22 18:26:53 +0400

    lesson 19 added

---
+++ Lesson 19th toDos/toDo1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+
+namespace toDo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread method1Runner = new(Method1);
+            method1Runner.Start();
+
+            Thread method2Runner = new(Method2);
+            method2Runner.Start();
+
+        }
+
+        public static int helper = 0;
+
+        public static void Method1()
+        {
+            Console.WriteLine("Enter a number you want to make stopping point counting from 1\nbtw this is Method1 :)\n");
+            var n = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (helper == 0)
+                {
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("\nMethod1 " + i);
+                Thread.Sleep(50);
+            }
+        }
+
+        public static void Method2()
+        {
+            Console.WriteLine("Enter a number you want to make stopping point counting from 1\nand this is Method2 :)" +
+                "\n\nP.S. Two method will start at the same time");
+            var n = int.Parse(Console.ReadLine());
+
+            if (n != null)
+            {
+                helper++;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("\nMethod2 " + i);
+                Thread.Sleep(50);
+            }
+        }
+
+    }
+}
---
+++ Lesson 19th toDos/toDo2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace toDo2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string catFactUrl = "https://catfact.ninja/fact";
+
+            Console.WriteLine("You will get a fact about cats\n");
+
+            while (true)
+            {
+                HttpClient client = new();
+                var result = await client.GetStringAsync(catFactUrl);
+                var catFact = JsonConvert.DeserializeObject<CatFact>(result);
+
+                Console.WriteLine(catFact.Fact + "\n");
+                Thread.Sleep(1000);
+                Console.WriteLine("If you wish hit enter to get more infos about cats");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+
+    public class CatFact
+    {
+        public string Fact { get; set; }
+        public int Lenght { get; set; }
+    }
+}
