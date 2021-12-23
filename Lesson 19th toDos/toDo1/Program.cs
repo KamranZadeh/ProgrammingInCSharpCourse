86249e8e242ba92dd20e7d5318951fe1a7e7ba13
KamranZadeh <Shixalizadekamran@gmail.com>
2021-12-23 17:09:05 +0400

    lesson 19 modified

--- Lesson 19th toDos/toDo1/Program.cs
+++ Lesson 19th toDos/toDo1/Program.cs
@@ -7,36 +7,22 @@ namespace toDo1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number you want to make stopping point counting from 1\nbtw this is Method1 :)\n");
+            var n1 = int.Parse(Console.ReadLine());
 
-            Thread method1Runner = new(Method1);
+            Console.WriteLine("\nEnter a number you want to make stopping point counting from 1\nand this is Method2 :)" +
+                "\n\nP.S. Two method will start at the same time");
+            var n2 = int.Parse(Console.ReadLine());
+
+            Thread method1Runner = new(() => Method1(n1));
             method1Runner.Start();
 
-            Thread method2Runner = new(Method2);
+            Thread method2Runner = new(() => Method2(n2));
             method2Runner.Start();
-
         }
 
-        public static int helper = 0;
-
-        public static void Method1()
+        public static void Method1(int n)
         {
-            Console.WriteLine("Enter a number you want to make stopping point counting from 1\nbtw this is Method1 :)\n");
-            var n = int.Parse(Console.ReadLine());
-
-            while (true)
-            {
-                if (helper == 0)
-                {
-                    Thread.Sleep(200);
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            
-
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine("\nMethod1 " + i);
@@ -44,17 +30,8 @@ namespace toDo1
             }
         }
 
-        public static void Method2()
+        public static void Method2(int n)
         {
-            Console.WriteLine("Enter a number you want to make stopping point counting from 1\nand this is Method2 :)" +
-                "\n\nP.S. Two method will start at the same time");
-            var n = int.Parse(Console.ReadLine());
-
-            if (n != null)
-            {
-                helper++;
-            }
-
             for (int i = 1; i <= n; i++)
             {
                 Console.WriteLine("\nMethod2 " + i);
