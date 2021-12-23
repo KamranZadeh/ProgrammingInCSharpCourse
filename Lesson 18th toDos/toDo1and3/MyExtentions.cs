49aaa7f219c17f8ac302fbae2d13474c86010482
KamranZadeh <Shixalizadekamran@gmail.com>
2021-12-23 15:36:03 +0400

    lesson 18 added

---
+++ Lesson 18th toDos/toDo1and3/MyExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace toDo1and3
+{
+    public static class MyExtentions
+    {
+        public static IEnumerable<TSource> WhereMy<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            return source.Where(predicate);
+        }
+
+        public static TSource? FirstOrDefaultMy<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.FirstOrDefault();
+        }
+
+        public static TSource? FirstOrDefaultMy<TSource> (this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            return source.FirstOrDefault(predicate);
+        }
+    }
+}
---
+++ Lesson 18th toDos/toDo1and3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+namespace toDo1and3 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table table = new();
+
+            // This indexer works. If you enter name that doesn't exits in the list if will throw OutOfRange exception.
+
+            Console.WriteLine(table[3].Name) ;
+            Console.WriteLine(table["Ann"].Name);
+
+        }
+
+    }
+
+    public class Guest
+    {
+        public string Name { get; set; }
+    }
+
+    public class Table
+    {
+        public Table()
+        {
+            Guests = new List<Guest>()
+            {
+                new Guest(){Name = "John"},   
+                new Guest(){Name = "Charlie"},
+                new Guest() {Name = "Jill"},
+                new Guest(){Name = "Jane"},
+                new Guest(){Name = "Martin"},
+                new Guest(){Name = "Ann"},
+                new Guest(){Name = "Eve"}
+            };
+        }
+
+        private List<Guest> Guests { get; set; }
+
+        public Guest this[int index]
+        {
+            get => Guests[index];
+            set => Guests[index] = value;
+        }
+
+        public Guest this[string name]
+        {
+            get
+            {
+                int i;
+
+                for (i = 0; i < Guests.Count; i++)
+                {
+                    if (name == Guests[i].Name)
+                    {
+                        i = i;
+                        break;
+                    }
+                    
+                }
+                return Guests[i];
+            }
+
+
+        }
+    }
+
+
+
+
+}
